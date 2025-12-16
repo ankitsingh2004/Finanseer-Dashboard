39d6cf13dcbc384111372355bc15b4f261b4bb88
Ankit Singh <as5806801@gmail.com>
2025-12-16 23:38:00 +0530

    Update kpi.js

--- server/routes/kpi.js
+++ server/routes/kpi.js
@@ -1,22 +1,3 @@
-// import express from "express";
-// import KPI from "../models/KPI.js";
-
-// const router = express.Router();
-
-// router.get("/kpis", async (req, res) => {
-//   try {
-//     const kpis = await KPI.find();
-//     res.status(200).json(kpis);
-//   } catch (error) {
-//     res.status(404).json({ message: error.message });
-//   }
-// });
-
-// export default router;
-
-
-
-
 import express from "express";
 import KPI from "../models/KPI.js";
 
