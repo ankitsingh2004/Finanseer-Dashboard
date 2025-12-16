2a072650ea96f12cd6aa44407716dbadd4ba1413
Ankit Singh <as5806801@gmail.com>
2025-12-16 23:37:39 +0530

    Update transaction.js

--- server/routes/transaction.js
+++ server/routes/transaction.js
@@ -1,25 +1,3 @@
-// import express from "express";
-// import Transaction from "../models/Transaction.js";
-
-// const router = express.Router();
-
-// router.get("/transactions", async (req, res) => {
-//   try {
-//     const transactions = await Transaction.find()
-//       .limit(50)
-//       .sort({ createdAt: -1 });
-
-
-//     res.status(200).json(transactions);
-//   } catch (error) {
-//     res.status(404).json({ message: error.message });
-//   }
-// });
-
-// export default router;
-
-
-
 
 import express from "express";
 import Transaction from "../models/Transaction.js";
@@ -30,7 +8,7 @@ router.get("/transactions", async (req, res) => {
   try {
     const transactions = await Transaction.find()
       .limit(50)
-      .sort({ createdAt: -1 }); // ✅ FIXED FIELD
+      .sort({ createdAt: -1 }); //  FIXED FIELD
 
     res.status(200).json(transactions);
   } catch (error) {
