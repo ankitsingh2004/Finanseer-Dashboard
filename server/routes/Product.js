559f6768a3962cf4bc9347947aef2c6d1ce729eb
Ankit Singh <as5806801@gmail.com>
2025-12-16 23:37:15 +0530

    Update Product.js

--- server/routes/Product.js
+++ server/routes/Product.js
@@ -1,20 +1,3 @@
-// import express from "express";
-// import Product from "../models/Product.js";
-
-// const router = express.Router();
-
-// router.get("/products", async (req, res) => {
-//   try {
-//     const products = await Product.find();
-//     res.status(200).json(products);
-//   } catch (error) {
-//     res.status(404).json({ message: error.message });
-//   }
-// });
-
-// export default router;
-
-
 
 import express from "express";
 import Product from "../models/Product.js";
